d2bc4ca219b13e73d253b894886e46ec5c2f4ef7
zyaxc <massnam2@gmail.com>
2022-09-16 14:55:49 +0700

    initial commit

---
+++ assets/scripts/Scripts.js
@@ -0,0 +1,4 @@
+setTimeout(()=>{
+    const animation = document.getElementById('main')
+    animation.style.opacity = 1
+},1000)
---
+++ assets/scripts/Vanta.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded',()=>{
+    VANTA.BIRDS({
+        el: "#vanta--js",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        birdSize: 1.50,
+        wingSpan: 14.00,
+        speedLimit: 4.00,
+        separation: 30.00,
+        alignment: 35.00,
+        color1: 0x0,
+        color2: 0xd1ff,
+        colorMode: "lerpGradient",
+        backgroundAlpha: 0.0
+        })
+})
